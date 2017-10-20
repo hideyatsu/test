78a641136357443bcce7a3aad44f855dd775acb9
hideyatsu <hideyatsuapp@gmail.com>
2017-10-20 16:33:01 +0700

    Add default route for hideyatsu

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,11 @@
 <?php
 
 Route::group(['prefix' => 'hideyatsu'], function () {
-    Route::get('test', function(){
-    	echo 'Hello from the test package!';
+    Route::get('/', function () {
+        echo 'Hello World!';
+    });
+    Route::get('test', function() {
+        echo 'Hello from the test package!';
     });
     Route::get('upper/{str}', 'Hideyatsu\Test\TestController@upper');
     Route::get('lower/{str}', 'Hideyatsu\Test\TestController@lower');
